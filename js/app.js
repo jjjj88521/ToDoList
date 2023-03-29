91e1da2f1b4f2f0b6dcaf213eb4d0a117a87a546
jjjj88521 <jjjj88521@gmail.com>
2023-03-29 23:38:24 +0800

    add and delete

--- js/app.js
+++ js/app.js
@@ -11,40 +11,107 @@ const dataArr = localStorage.getItem("todo-data")
 
 // 獲取 todo-list 區塊
 const todoListDiv = document.querySelector(".todo-list");
-
+// 一開始先渲染一次
+renderList();
+countTodo();
+// 加號按下去就輸入
 addBtn.addEventListener("click", () => {
   if (newTodo.value === "") {
     alert("不得為空!!!");
   } else {
     // 創建物件 用來獲取輸入
     const todoObj = {
-      id: dataArr.length ? dataArr[dataArr.length - 1].id + 1 : 1,
+      id: dataArr.length ? dataArr[dataArr.length - 1].id + 1 : 0,
       title: newTodo.value,
       completed: false,
     };
     // 將物件 push 進 dataArr
     dataArr.push(todoObj);
     console.log(dataArr);
+    // 清空input
     newTodo.value = "";
 
     // 渲染頁面
     renderList();
+    countTodo();
+  }
+});
+// enter按下去也輸入
+newTodo.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    if (newTodo.value === "") {
+      alert("不得為空!!!");
+    } else {
+      // 創建物件 用來獲取輸入
+      const todoObj = {
+        id: dataArr.length ? dataArr[dataArr.length - 1].id + 1 : 0,
+        title: newTodo.value,
+        completed: false,
+      };
+      // 將物件 push 進 dataArr
+      dataArr.push(todoObj);
+      console.log(dataArr);
+      // 清空input
+      newTodo.value = "";
+
+      // 渲染頁面
+      renderList();
+      countTodo();
+    }
+  }
+});
+
+// 代辦事項打勾 文字內容就槓掉
+todoListDiv.addEventListener("change", function (e) {
+  // 用兄弟節點找到 span 文字內容
+  const text = e.target.nextElementSibling;
+  // 如果對應的資料的completed屬性為false就槓掉 否則設定空字串(預設值)
+  text.style.textDecorationLine = !dataArr[e.target.dataset.id].completed
+    ? "line-through"
+    : "";
+  text.style.color = !dataArr[e.target.dataset.id].completed ? "gray" : "";
+  dataArr[e.target.dataset.id].completed =
+    !dataArr[e.target.dataset.id].completed;
+  countTodo();
+});
+
+// 按下垃圾 刪掉相對應的事項
+todoListDiv.addEventListener("click", function (e) {
+  if (e.target.tagName === "BUTTON") {
+    // console.log(e.target);
+    // 刪除相對應的事項
+    dataArr.splice(e.target.dataset.id, 1);
+    // 渲染
+    renderList();
+    countTodo();
   }
 });
 
 function renderList() {
   // 清空 為了重新渲染
-  todoListDiv.innerHTML = "";
+  todoListDiv.innerHTML =
+    dataArr.length === 0 ? `<p class="no-todo">沒事情了耶！！！！</p>` : "";
   for (let i = 0; i < dataArr.length; i++) {
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
     todoDiv.innerHTML = `
-          <input class="toggle" type="checkbox" />
+          <input class="toggle" type="checkbox" data-id=${i} />
           <span>${dataArr[i].title}</span>
-          <button class="trash">
+          <button class="trash" data-id=${i}>
             <i class="fas fa-trash"></i>
           </button>
         `;
     todoListDiv.appendChild(todoDiv);
   }
+}
+
+// 計算還有多少個事項未完成
+function countTodo() {
+  // 獲取 done-count 區塊
+  const todoP = document.querySelector(".todo-count");
+  let c = 0;
+  dataArr.forEach((e) => {
+    c = e.completed ? c : c + 1;
+  });
+  todoP.innerHTML = `<span>還有${c}件事沒做完</span>`;
 }
